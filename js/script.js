90fd3c8e07da3b1c4d71f15d8d0ead4eee63d66f
Daniel-leiken <DanStr16221@stud.noroff.no>
2023-09-09 20:15:56 +0200

    API updates
    API calls from first row of "List of games" finished.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const url = "https://api.noroff.dev/api/v1/gamehub";
 
-const resultsContainer = document.querySelector(".results");
+const productContainer = document.querySelector(".game-row");
 
 async function getGames () {
 
@@ -8,7 +8,7 @@ async function getGames () {
 
     const results = await response.json();
 
-    resultsContainer.innerHTML = "";
+    productContainer.innerHTML = "";
     
     for(let i = 0; i < results.length; i++) {
         console.log(results[i].title);
@@ -18,9 +18,9 @@ async function getGames () {
         }
        
        //resultsContainer.innerHTML += `<div class="game-item">${results[i].title}</div> <div class="game-description">hello</div>`;
-       resultsContainer.innerHTML += `<a href="productpage.html">
+       productContainer.innerHTML += `<a href="productpage.html">
                     <div class="game-item">
-                        <img src="/images/GameHub_covers3.jpg" alt="Black"/>
+                        <img src="${results[i].image}" alt="Black"/>
                         <div class="game-description">
                             <p>${results[i].title}</p>
                             <div class="platforms">
@@ -30,8 +30,8 @@ async function getGames () {
                             </div>
                         </div>
                         <div class="game-price">
-                            <span class="old-price">59,99$</span>
-                            <span class="new-price">39,99$</span>
+                            <span class="old-price">${results[i].price}$</span>
+                            <span class="new-price">${results[i].discountedPrice}$</span>
                         </div>
                     </div>
                 </a>`;
