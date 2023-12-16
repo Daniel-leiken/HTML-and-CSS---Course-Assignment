a795275e619efc537b3b7c662e7f5c1f417e2f6c
Daniel Leiken Strandheim <117750912+Daniel-leiken@users.noreply.github.com>
2023-12-16 18:52:13 +0100

    Update to Hotjar script

--- js/hotjar.js
+++ js/hotjar.js
@@ -1,4 +1,4 @@
-<script>
+
     (function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
         h._hjSettings={hjid:3797572,hjsv:6};
@@ -7,4 +7,3 @@
         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
         a.appendChild(r);
     })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-</script>
